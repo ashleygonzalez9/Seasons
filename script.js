76ccda331e431d15bdaee56725c9a65b4dad3158
ashleygonzalez9 <37843561+ashleygonzalez9@users.noreply.github.com>
2019-11-08 15:40:06 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+// call this function when "orange-kid" is clicked!
+
+
+
+function moveGif(elem){
+	elem.classList.add("fly-forward");
+}
+
+// call this function when "blue-man" is clicked!
+function changePicture(elem){
+	elem.src = "images/vegeta1.png";
+}
